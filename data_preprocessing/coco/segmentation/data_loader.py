83c9d39f73b56a7fe6791ae5606ae66fe4e59164
Adarsh <adarshan_naiynar@icloud.com>
2020-12-17 19:05:17 -0800

    Refactored data loader variable names

--- data_preprocessing/coco/segmentation/data_loader.py
+++ data_preprocessing/coco/segmentation/data_loader.py
@@ -1,20 +1,14 @@
-import logging
-
-import torch.utils.data as data
 import numpy as np
-
+import torch.utils.data as data
 from torchvision import transforms
-from data_preprocessing.coco.segmentation.transforms import Normalize, ToTensor, FixedResize
-from data_preprocessing.coco.segmentation.datasets import CocoSegmentation
+
 from FedML.fedml_core.non_iid_partition.noniid_partition import record_data_stats, \
     partition_class_samples_with_dirichlet_distribution
-
-logging.basicConfig()
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from .datasets import CocoSegmentation
+from .transforms import Normalize, ToTensor, FixedResize
 
 
-def _data_transforms_coco():
+def _data_transforms_coco_segmentation():
     COCO_MEAN = (0.485, 0.456, 0.406)
     COCO_STD = (0.229, 0.224, 0.225)
 
@@ -28,51 +22,49 @@ def _data_transforms_coco():
 
 
 # for centralized training
-def get_dataloader(dataset, datadir, train_bs, test_bs, dataidxs=None):
-    return get_dataloader_coco(datadir, train_bs, test_bs, dataidxs)
+def get_dataloader(_, data_dir, train_bs, test_bs, data_idxs=None):
+    return get_dataloader_coco_segmentation(data_dir, train_bs, test_bs, data_idxs)
 
 
 # for local devices
-def get_dataloader_test(datadir, train_bs, test_bs, dataidxs_train, dataidxs_test):
-    return get_dataloader_coco_test(datadir, train_bs, test_bs, dataidxs_train, dataidxs_test)
+def get_dataloader_test(data_dir, train_bs, test_bs, data_idxs_train, data_idxs_test):
+    return get_dataloader_coco_segmentation_test(data_dir, train_bs, test_bs, data_idxs_train, data_idxs_test)
 
 
-def get_dataloader_coco(datadir, train_bs, test_bs, dataidxs=None):
-    transform_train, transform_test = _data_transforms_coco()
+def get_dataloader_coco_segmentation(data_dir, train_bs, test_bs, data_idxs=None):
+    transform_train, transform_test = _data_transforms_coco_segmentation()
 
-    train_ds = CocoSegmentation(datadir,
+    train_ds = CocoSegmentation(data_dir,
                                 split='train',
                                 transform=transform_train,
                                 download_dataset=False,
-                                data_idxs=dataidxs)
-    test_ds = CocoSegmentation(datadir,
+                                data_idxs=data_idxs)
+
+    test_ds = CocoSegmentation(data_dir,
                                split='val',
                                transform=transform_test,
                                download_dataset=False)
 
-    print(len(train_ds))
-
     train_dl = data.DataLoader(dataset=train_ds, batch_size=train_bs, shuffle=True, drop_last=True)
     test_dl = data.DataLoader(dataset=test_ds, batch_size=test_bs, shuffle=False, drop_last=True)
 
     return train_dl, test_dl, train_ds.num_classes
 
 
-def get_dataloader_coco_test(datadir, train_bs, test_bs, dataidxs_train=None, dataidxs_test=None):
-    transform_train, transform_test = _data_transforms_coco()
+def get_dataloader_coco_segmentation_test(data_dir, train_bs, test_bs, data_idxs_train=None, data_idxs_test=None):
+    transform_train, transform_test = _data_transforms_coco_segmentation()
 
-    train_ds = CocoSegmentation(datadir,
+    train_ds = CocoSegmentation(data_dir,
                                 split='train',
                                 transform=transform_train,
                                 download_dataset=False,
-                                data_idxs=dataidxs_train)
-    test_ds = CocoSegmentation(datadir,
+                                data_idxs=data_idxs_train)
+
+    test_ds = CocoSegmentation(data_dir,
                                split='val',
                                transform=transform_test,
                                download_dataset=False,
-                               data_idxs=dataidxs_test)
-
-    print(len(train_ds))
+                               data_idxs=data_idxs_test)
 
     train_dl = data.DataLoader(dataset=train_ds, batch_size=train_bs, shuffle=True, drop_last=True)
     test_dl = data.DataLoader(dataset=test_ds, batch_size=test_bs, shuffle=False, drop_last=True)
@@ -80,28 +72,27 @@ def get_dataloader_coco_test(datadir, train_bs, test_bs, dataidxs_train=None, da
     return train_dl, test_dl, train_ds.num_classes
 
 
-def load_coco_data(datadir):
-    transform_train, transform_test = _data_transforms_coco()
+def load_coco_data(data_dir):
+    transform_train, transform_test = _data_transforms_coco_segmentation()
 
-    train_ds = CocoSegmentation(datadir, split='train', transform=transform_train, download_dataset=False)
-    test_ds = CocoSegmentation(datadir, split='val', transform=transform_test, download_dataset=False)
+    train_ds = CocoSegmentation(data_dir, split='train', transform=transform_train, download_dataset=False)
+    test_ds = CocoSegmentation(data_dir, split='val', transform=transform_test, download_dataset=False)
 
     return train_ds.img_ids, train_ds.target, train_ds.cat_ids, test_ds.img_ids, test_ds.target, test_ds.cat_ids
 
 
 # Get a partition map for each client
-def partition_data(datadir, partition, n_nets, alpha):
-    traindata_cls_counts = None
-    net_dataidx_map = None
-    logging.info("********* partition data COCO ***************")
-    train_images, train_targets, train_cat_ids, _, __, ___ = load_coco_data(datadir)
+def partition_data(data_dir, partition, n_nets, alpha):
+    net_data_idx_map = None
+    idx_batch = None
+    train_images, train_targets, train_cat_ids, _, __, ___ = load_coco_data(data_dir)
     n_train = len(train_images)  # Number of training samples
 
     if partition == "homo":
         total_num = n_train
         idxs = np.random.permutation(total_num)
         batch_idxs = np.array_split(idxs, n_nets)  # As many splits as n_nets = number of clients
-        net_dataidx_map = {i: batch_idxs[i] for i in range(n_nets)}
+        net_data_idx_map = {i: batch_idxs[i] for i in range(n_nets)}
 
     # non-iid data distribution
     # TODO: Add custom non-iid distribution option - hetero-fix
@@ -109,7 +100,7 @@ def partition_data(datadir, partition, n_nets, alpha):
         min_size = 0
         categories = train_cat_ids  # category names
         N = n_train  # Number of labels/training samples
-        net_dataidx_map = {}
+        net_data_idx_map = {}
 
         while min_size < 10:
             # Create a list of empty lists for clients
@@ -131,40 +122,35 @@ def partition_data(datadir, partition, n_nets, alpha):
 
         for j in range(n_nets):
             np.random.shuffle(idx_batch[j])
-            net_dataidx_map[j] = idx_batch[j]
+            net_data_idx_map[j] = idx_batch[j]
 
-    traindata_cls_counts = record_data_stats(train_targets, net_dataidx_map, is_segmentation=True)
+    train_data_cls_counts = record_data_stats(train_targets, net_data_idx_map, is_segmentation=True)
 
-    return net_dataidx_map, traindata_cls_counts
+    return net_data_idx_map, train_data_cls_counts
 
 
-def load_partition_data_distributed_coco(process_id, dataset, data_dir, partition_method, partition_alpha,
-                                         client_number, batch_size):
-    net_dataidx_map, traindata_cls_counts = partition_data(data_dir,
-                                                           partition_method,
-                                                           client_number,
-                                                           partition_alpha)
-    logging.info("traindata_cls_counts = " + str(traindata_cls_counts))
-    train_data_num = sum([len(net_dataidx_map[r]) for r in range(client_number)])
+def load_partition_data_distributed_coco_segmentation(process_id, dataset, data_dir, partition_method, partition_alpha,
+                                                      client_number, batch_size):
+    net_data_idx_map, train_data_cls_counts = partition_data(data_dir,
+                                                             partition_method,
+                                                             client_number,
+                                                             partition_alpha)
+
+    train_data_num = sum([len(net_data_idx_map[r]) for r in range(client_number)])
 
     # get global test data
     if process_id == 0:
         train_data_global, test_data_global, class_num = get_dataloader(dataset, data_dir, batch_size, batch_size)
-        logging.info("train_dl_global number = " + str(len(train_data_global)))
-        logging.info("test_dl_global number = " + str(len(test_data_global)))
         train_data_local_dict = None
         test_data_local_dict = None
         data_local_num_dict = None
     else:
         # get local dataset
         client_id = process_id - 1
-        dataidxs = net_dataidx_map[client_id]
-        local_data_num = len(dataidxs)
-        logging.info("rank = %d, local_sample_number = %d" % (process_id, local_data_num))
+        data_idxs = net_data_idx_map[client_id]
+        local_data_num = len(data_idxs)
         train_data_local, test_data_local, class_num = get_dataloader(dataset, data_dir, batch_size, batch_size,
-                                                                      dataidxs)
-        logging.info("process_id = %d, batch_num_train_local = %d, batch_num_test_local = %d" % (
-            process_id, len(train_data_local), len(test_data_local)))
+                                                                      data_idxs)
 
         data_local_num_dict = {client_id: local_data_num}
         train_data_local_dict = {client_id: train_data_local}
@@ -176,19 +162,17 @@ def load_partition_data_distributed_coco(process_id, dataset, data_dir, partitio
 
 
 # Called from main_fedseg
-def load_partition_data_coco(dataset, data_dir, partition_method, partition_alpha, client_number, batch_size):
-    net_dataidx_map, traindata_cls_counts = partition_data(data_dir,
-                                                           partition_method,
-                                                           client_number,
-                                                           partition_alpha)
+def load_partition_data_coco_segmentation(dataset, data_dir, partition_method, partition_alpha, client_number,
+                                          batch_size):
+    net_data_idx_map, train_data_cls_counts = partition_data(data_dir,
+                                                             partition_method,
+                                                             client_number,
+                                                             partition_alpha)
 
-    logging.info("traindata_cls_counts = " + str(traindata_cls_counts))
-    train_data_num = sum([len(net_dataidx_map[r]) for r in range(client_number)])
+    train_data_num = sum([len(net_data_idx_map[r]) for r in range(client_number)])
 
     # Global train and test data
     train_data_global, test_data_global, class_num = get_dataloader(dataset, data_dir, batch_size, batch_size)
-    logging.info("train_dl_global number = " + str(len(train_data_global)))
-    logging.info("test_dl_global number = " + str(len(test_data_global)))
     test_data_num = len(test_data_global)
 
     # get local dataset
@@ -197,15 +181,12 @@ def load_partition_data_coco(dataset, data_dir, partition_method, partition_alph
     test_data_local_dict = dict()
 
     for client_idx in range(client_number):
-        dataidxs = net_dataidx_map[client_idx]
-        local_data_num = len(dataidxs)
+        data_idxs = net_data_idx_map[client_idx]
+        local_data_num = len(data_idxs)
         data_local_num_dict[client_idx] = local_data_num
-        logging.info("client_idx = %d, local_sample_number = %d" % (client_idx, local_data_num))
 
         train_data_local, test_data_local, class_num = get_dataloader(dataset, data_dir, batch_size, batch_size,
-                                                                      dataidxs)
-        logging.info("client_idx = %d, batch_num_train_local = %d, batch_num_test_local = %d" % (
-            client_idx, len(train_data_local), len(test_data_local)))
+                                                                      data_idxs)
 
         # Store dataloaders for each client as they contain specific data
         train_data_local_dict[client_idx] = train_data_local
