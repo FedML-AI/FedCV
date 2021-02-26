01c8a7fcabca436339574c4691f2a3f480aa5a9a
Adarsh <adarshan_naiynar@icloud.com>
2021-02-25 19:24:29 -0800

    Adding train,val,test dataset download and target processing code

--- data/cityscapes/download_cityscapes.sh
+++ data/cityscapes/download_cityscapes.sh
@@ -4,7 +4,7 @@ USERNAME=$1
 PASSWORD=$2
 
 echo "Logging in using the credentials..."
-wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=sivashun@usc.edu&password=fedcvteam&submit=Login' https://www.cityscapes-dataset.com/login/
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=$USERNAME&password=$PASSWORD&submit=Login' https://www.cityscapes-dataset.com/login/
 rm index.html
 
 echo "Downloading gtCoarse.zip..."
@@ -12,9 +12,23 @@ wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dat
 echo "Extracting gtCoarse.zip..."
 unzip gtCoarse.zip
 rm gtCoarse.zip
+rm README*
+rm license.txt
+
+echo "Downloading leftImg8bit_trainvaltest.zip"
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
+echo "Extracting leftImg8bit_trainvaltest.zip"
+unzip leftImg8bit_trainvaltest.zip
+rm leftImg8bit_trainvaltest.zip
+rm README*
+rm license.txt
 
 echo "Downloading leftImg8bit_trainextra.zip"
 wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=4
 echo "Extracting leftImg8bit_trainextra.zip"
 unzip leftImg8bit_trainextra.zip
 rm leftImg8bit_trainextra.zip
+rm README*
+rm license.txt
+
+python process_targets.py
