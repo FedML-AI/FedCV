81f43a0c3c7ec5922413e1e80ec84e6fdd27c8df
TANG Zhenheng <20481896@scigpu11.comp.hkbu.edu.hk>
2021-03-06 17:30:00 +0800

    add pretrained model

---
+++ model/classification/pretrained/download_mobilenetV3_pretrained_weights.sh
@@ -0,0 +1,5 @@
+wget https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/mobilenetv3_large_100_ra-f55367f5.pth
+
+# refer to https://github.com/rwightman/pytorch-image-models/blob/5fcdd4b822fc6cf2166e1eac2914ed34e3b2a731/timm/models/mobilenetv3.py
+
+
