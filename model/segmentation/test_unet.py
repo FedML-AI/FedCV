3b44963b5e8a2aa161f3ecd484d9fcd3a108755f
Adarsh <adarshan_naiynar@icloud.com>
2021-03-10 14:11:09 -0800

    Fixing __flops__ warnings

--- model/segmentation/test_unet.py
+++ model/segmentation/test_unet.py
@@ -2,20 +2,21 @@ from ptflops import get_model_complexity_info
 
 from model.segmentation.unet import UNet
 
-model = UNet(pretrained=True)
+if __name__ == '__main__':
+    print('================================================================================')
+    print('UNet, ResNet, 512x512')
+    print('================================================================================')
+    model = UNet(pretrained=True)
+    flops, params = get_model_complexity_info(model, (3, 512, 512), verbose=True)
 
-print('================================================================================')
-print('UNet, ResNet, 512x512')
-print('================================================================================')
-flops, params = get_model_complexity_info(model, (3, 512, 512), verbose=True)
+    print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
+    print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 
-print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
-print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+    print('================================================================================')
+    print('UNet, ResNet, 768x768')
+    print('================================================================================')
+    model = UNet(pretrained=True)
+    flops, params = get_model_complexity_info(model, (3, 768, 768), verbose=True)
 
-print('================================================================================')
-print('UNet, ResNet, 768x768')
-print('================================================================================')
-flops, params = get_model_complexity_info(model, (3, 768, 768), verbose=True)
-
-print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
-print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+    print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
+    print('{:<30}  {:<8}'.format('Number of parameters: ', params))
